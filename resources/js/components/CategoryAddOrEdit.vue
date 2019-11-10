bf2eabe61cc5b5b22dcd3bff829d840287a6ede7
Benjamin Vargas <bvj2206@gmail.com>
2019-11-10 19:47:58 -0400

    improve category add or edit logic

--- resources/js/components/CategoryAddOrEdit.vue
+++ resources/js/components/CategoryAddOrEdit.vue
@@ -1,13 +1,76 @@
 <template>
-    <form action="">
-        <div class="form-row">
-            <label for="">Name</label>
-            <input type="text" class="form-control">
-        </div>
-    </form>
+  <form action @submit.prevent>
+    <div class="form-group">
+      <label for>Name</label>
+      <input type="text" class="form-control" v-model="category.name" />
+    </div>
+    <div class="form-group">
+      <button class="btn btn-success" @click="add" v-if="mode === 'add'">Add New Category</button>
+      <button class="btn btn-success" @click="update" v-else>Update category</button>
+    </div>
+  </form>
 </template>
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      category: {
+        name: ""
+      }
+    };
+  },
+  props: {
+    itemId: {
+      type: Number,
+      default: null
+    },
+    csrf: {
+      type: String,
+      default: null
+    },
+    mode: {
+      type: String,
+      default: "add"
+    },
+    parent: {
+      type: Object,
+      default: null
+    }
+  },
+  mounted() {
+    if (this.mode == "edit") {
+      this.loadData();
+    }
+  },
+  methods: {
+    async loadData() {
+      let response = await this.$http.get(`ajax/categories/${this.itemId}`);
+      this.category = response.data;
+    },
+    async add() {
+      await this.$http({
+        method: "post",
+        data: this.category,
+        url: "ajax/categories/add",
+        headers: {
+          "X-CSRF-TOKEN": this.csrf
+        }
+      });
+      this.$root.$emit("update-data");
+      this.parent.hide();
+    },
+    async update() {
+      await this.$http({
+        method: "put",
+        data: this.category,
+        url: `ajax/categories/${this.itemId}/update`,
+        headers: {
+          "X-CSRF-TOKEN": this.csrf
+        }
+      });
+      this.$root.$emit("update-data");
+      this.parent.hide();
+    }
+  }
+};
 </script>
